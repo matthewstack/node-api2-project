8ccf13d1cb4b5e6177e038a4cb708d282010d382
Matt <86365526+matthewstack@users.noreply.github.com>
2022-02-06 18:46:12 -0500

    server, index, and router setup

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,26 @@
 // implement your posts router here
+const Posts = require("./posts-model");
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const thePosts = await Posts.find();
+    res.json(thePosts);
+  } catch (err) {
+    res.status(500).json({
+      message: "The posts information could not be retrieved",
+    });
+  }
+});
+
+// router.get("/:id", async (req, res) => {
+//     try {
+//     } catch (err) {}
+//   });
+
+//   router.post("/", async (req, res) => {
+//     try {
+//     } catch (err) {}
+//   });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // require your server and launch it here
+const server = require("./api/server");
+PORT = 4000;
+server.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
